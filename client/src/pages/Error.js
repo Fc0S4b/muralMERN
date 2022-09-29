186bc35ddb324cb8d06e2ed737eb7f9253a1baaa
Fc0S4b <fcoj.sabugo@gmail.com>
2022-09-29 00:31:25 -0300

    errorPage

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,14 +1,16 @@
 import { Link } from 'react-router-dom';
 import img from '../assets/images/not-found.svg';
-
+import Wrapper from '../assets/wrappers/ErrorWrapper';
 const Error = () => {
   return (
-    <div>
-      <img src={img} alt="not found" />
-      <h3>Error</h3>
-      <p>Not Found!</p>
-      <Link to="/">back home</Link>
-    </div>
+    <Wrapper className="full-page">
+      <div>
+        <img src={img} alt="not found" />
+        <h3>Error, página no encontrada</h3>
+        <p>No podemos encontrar la página que buscas.</p>
+        <Link to="/">ir al inicio</Link>
+      </div>
+    </Wrapper>
   );
 };
 export default Error;
