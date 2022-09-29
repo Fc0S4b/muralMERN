5411e975396933e7abd4816dd4a58f36618ccb32
Fc0S4b <fcoj.sabugo@gmail.com>
2022-09-29 02:45:23 -0300

    Landing page

---
+++ client/src/assets/wrappers/LandingWrapper.js
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.main`
+  background: var(--color-2-100);
+  display: grid;
+  place-content: center;
+  .page {
+    display: flex;
+    justify-content: space-between;
+    gap: 50px;
+  }
+  img {
+    max-width: 600px;
+    display: block;
+  }
+  p {
+    font-size: 1.5rem;
+    letter-spacing: 0.1rem;
+  }
+  .info {
+    text-align: left;
+  }
+
+  span {
+    color: var(--primary-500);
+  }
+  p {
+    font-size: 1.5rem;
+    letter-spacing: 0.1rem;
+    margin-bottom: 15px;
+  }
+
+  @media (max-width: 992px) {
+    position: relative;
+    background: var(--color-2-100) url('../images/main.svg') no-repeat
+      center/cover;
+
+    img {
+      opacity: 16%;
+      display: block;
+      position: absolute;
+      top: -12%;
+      left: -1%;
+      width: 600px;
+      height: 600px;
+    }
+
+    .info {
+      position: absolute;
+      top: 20%;
+      left: 50%;
+      transform: translateX(-50%);
+      text-align: center;
+    }
+    p {
+      max-width: 500px;
+      margin: 30px auto;
+    }
+  }
+`;
+
+export default Wrapper;
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,6 +1,27 @@
 import { Link } from 'react-router-dom';
+import main from '../assets/images/main.svg';
+import Wrapper from '../assets/wrappers/LandingWrapper';
 
 const Landing = () => {
-  return <Link to="/register">Login/Register</Link>;
+  return (
+    <Wrapper>
+      <div className="container page full-page">
+        <img src={main} alt="add post" className="img main-img" />
+        <div className="info">
+          <h1>
+            <span>Mural</span> de noticias
+          </h1>
+          <p>
+            Crea tu propia bitácora de noticias personalizadas y gestiona la
+            información que consideres relevante para compartirlas con tu propia
+            comunidad.
+          </p>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 export default Landing;
