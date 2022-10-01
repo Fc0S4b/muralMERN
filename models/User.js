33c76b158e5b1325326c974be423dc9028811fa8
Fc0S4b <fcoj.sabugo@gmail.com>
2022-10-01 13:55:50 -0300

    User model

---
+++ models/User.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+import validator from 'validator';
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Por favor escriba su nombre'],
+    minlength: 3,
+    maxlength: 20,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Por favor escriba su email'],
+    validate: {
+      validator: validator.isEmail,
+      message: 'Por favor escriba un email válido',
+    },
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Por favor escriba una contraseña'],
+    minlength: 6,
+  },
+  lastName: {
+    type: String,
+    maxlength: 20,
+    trim: true,
+    default: 'Apellido',
+  },
+  location: {
+    type: String,
+    trim: true,
+    maxlength: 20,
+    default: 'mi localidad',
+  },
+});
+
+export default mongoose.model('User', UserSchema);
