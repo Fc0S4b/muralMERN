e7181cafba1b240fba4bcaf2b9ba93aa428b42e2
Fc0S4b <fcoj.sabugo@gmail.com>
2022-10-06 23:06:45 -0300

    newModel cretedController

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -1,5 +1,16 @@
+import New from '../models/New.js';
+import { StatusCodes } from 'http-status-codes';
+import { BadRequestError, NotFoundError } from '../errors/index.js';
+
 const createNew = async (req, res) => {
-  return res.send('create new');
+  const { headline, description } = req.body;
+  if (!headline || !description) {
+    throw new BadRequestError('Por favor provea todos los valores');
+  }
+  req.body.createdBy = req.user.userId;
+
+  const newPost = await New.create(req.body);
+  res.status(StatusCodes.CREATED).json({ newPost });
 };
 const deleteNew = async (req, res) => {
   return res.send('delete new');
---
+++ models/New.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+// status [pendiente,lista, compartida  ]
+// tipo de noticia [saludo, información, noticia, reseña, artículo ,otro]
+// localidad
+// Títular
+// descripcion
+const NewSchema = new mongoose.Schema(
+  {
+    headline: {
+      type: String,
+      required: [true, 'Por favor provea un titular'],
+      maxlength: 50,
+    },
+    description: {
+      type: String,
+      required: [true, 'Por favor provea una descripción'],
+      maxlength: 100,
+    },
+    status: {
+      type: String,
+      enum: ['pendiente', 'lista', 'compartida', 'caducada'],
+      default: 'pendiente',
+    },
+    newType: {
+      type: String,
+      enum: ['saludo', 'información', 'noticia', 'reseña', 'artículo', 'otro'],
+      default: 'información',
+    },
+    newLocation: {
+      type: String,
+      default: 'mi localidad',
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Por favor provea un usuario'],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('New', NewSchema);
